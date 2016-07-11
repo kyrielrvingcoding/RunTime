1603617986232c581ab2652791b72365268936e9
kyrielrvingcoding <kyrie.lrving.coding@gmail.com>
2016-07-11 14:31:30 +0800

    runtime类和对象

---
+++ RunTime/RunTime/MyClass.h
@@ -0,0 +1,13 @@
+//
+//  MyClass.h
+//  RunTime
+//
+//  Created by 诸超杰 on 16/7/11.
+//  Copyright © 2016年 runTime.dome. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyClass : NSObject
+
+@end
---
+++ RunTime/RunTime/MyClass.m
@@ -0,0 +1,13 @@
+//
+//  MyClass.m
+//  RunTime
+//
+//  Created by 诸超杰 on 16/7/11.
+//  Copyright © 2016年 runTime.dome. All rights reserved.
+//
+
+#import "MyClass.h"
+
+@implementation MyClass
+
+@end
